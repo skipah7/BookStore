5f5efc7ea5dcb94e0727a1cfa1702f027efb4a5f
skiper76 <skiper-x00@mail.ru>
2021-07-14 05:28:45 +0400

    Example extension

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,46 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'BookStore';
+
+  books = [
+    {
+      name: 'The Psychopath test',
+      author: 'Jon Ronson',
+      releaseDate: '12.05.2011',
+      isbn: '978-1594485756',
+      coverPath: 'psychopath-test.jpg',
+      availableAmount: 5,
+      price: 2754
+    },
+    {
+      name: 'Dead Astronauts',
+      author: 'Jeff VanderMeer',
+      releaseDate: '03.12.2019', 
+      isbn: '978-0374276805',
+      coverPath: 'dead-astronauts.jpg',
+      availableAmount: 3,
+      price: 5325
+    },
+    {
+      name: 'The Everlasting',
+      author: 'Katy Simpson Smith',
+      releaseDate: '24.03.2020', 
+      isbn: '978-0062873644',
+      coverPath: 'The-Everlasting.jpg',
+      availableAmount: 6,
+      price: 9638
+    },
+    {
+      name: 'Witches of America',
+      author: 'Alex Mar',
+      releaseDate: '20.10.2015', 
+      isbn: '037-4291373',
+      coverPath: 'witches-of-america.jpg',
+      availableAmount: 1,
+      price: 1728
+    }
+  ]
 }
