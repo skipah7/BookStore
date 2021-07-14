19502ca131214d5f2690268f7a198e0e8c51f888
skiper76 <skiper-x00@mail.ru>
2021-07-14 05:48:24 +0400

    Made placeholders for future server request, emulated simple server response

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { books } from './source-placeholder'
 
 @Component({
   selector: 'app-root',
@@ -7,42 +8,13 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'BookStore';
 
-  books = [
-    {
-      name: 'The Psychopath test',
-      author: 'Jon Ronson',
-      releaseDate: '12.05.2011',
-      isbn: '978-1594485756',
-      coverPath: 'psychopath-test.jpg',
-      availableAmount: 5,
-      price: 2754
-    },
-    {
-      name: 'Dead Astronauts',
-      author: 'Jeff VanderMeer',
-      releaseDate: '03.12.2019', 
-      isbn: '978-0374276805',
-      coverPath: 'dead-astronauts.jpg',
-      availableAmount: 3,
-      price: 5325
-    },
-    {
-      name: 'The Everlasting',
-      author: 'Katy Simpson Smith',
-      releaseDate: '24.03.2020', 
-      isbn: '978-0062873644',
-      coverPath: 'The-Everlasting.jpg',
-      availableAmount: 6,
-      price: 9638
-    },
-    {
-      name: 'Witches of America',
-      author: 'Alex Mar',
-      releaseDate: '20.10.2015', 
-      isbn: '037-4291373',
-      coverPath: 'witches-of-america.jpg',
-      availableAmount: 1,
-      price: 1728
-    }
-  ]
+  books = books
+
+  addToCart(bookName: string) {
+    window.alert(`${bookName} will be added to shopping cart. Placeholder for now`)
+  }
+
+  refreshBooks() {
+    window.alert('Website will make a server request and refresh books on page. Placeholder for now')
+  }
 }
